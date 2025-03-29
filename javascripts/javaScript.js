afe84c6c0d3c1cb5c4c7ddde058c3f200f2bdec3
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-03-30 06:27:02 +0700

    Make square change color when mouse hover
    Make square change color when mouse hover and addng button for edit
    how many grid user want, css changing 6% to calc and styling the button

--- javascripts/javaScript.js
+++ javascripts/javaScript.js
@@ -1,9 +1,19 @@
 const squareContainer = document.querySelector(".container");
+const editBtn = document.querySelector(".editGridBtn");
+
+let grid = 0;
+document.addEventListener("DOMContentLoaded", function () {
+  editBtn.addEventListener("click", () => {
+    grid = Number(prompt("Enter how many grid you want ! ", 16)) ** 2;  
+  });
+});
 
 for (let i = 0; i < 256; i++) {
-    const squareGrid = document.createElement("div");
-    squareGrid.classList.add("grid");
-  squareGrid.setAttribute("id", i+1)
+  const squareGrid = document.createElement("div");
+  squareGrid.classList.add("grid");
+  squareGrid.setAttribute("id", i + 1);
   squareContainer.appendChild(squareGrid);
-  
+  squareGrid.addEventListener("mouseenter", () => {
+    squareGrid.style.backgroundColor = "black";
+  });
 }
