5738852d28a8451407d1a8cc371d63eca70d60a3
ihyaulhaq <138434786+ihyaulhaq@users.noreply.github.com>
2025-04-07 20:22:57 +0700

    Making existing grid removed after new input
    Clear existing grid after user input the number so the grid doesnt stack
    using the innerHtml empty before assigning the loop for the grid

--- javascripts/javaScript.js
+++ javascripts/javaScript.js
@@ -2,18 +2,31 @@ const squareContainer = document.querySelector(".container");
 const editBtn = document.querySelector(".editGridBtn");
 
 let grid = 0;
+
+function makeGrid(grid) {
+  squareContainer.innerHTML = "";
+  for (let i = 0; i < grid ** 2; i++) {
+    const squareGrid = document.createElement("div");
+    squareGrid.classList.add("grid");
+    squareGrid.setAttribute("id", i + 1);
+    squareContainer.appendChild(squareGrid);
+    squareGrid.style.width = `calc(100% / ${grid})`;
+    squareGrid.style.height = `calc(100% / ${grid})`;
+    squareGrid.addEventListener("mouseenter", () => {
+      squareGrid.style.backgroundColor = "black";
+    });
+  }
+}
+
 document.addEventListener("DOMContentLoaded", function () {
   editBtn.addEventListener("click", () => {
-    grid = Number(prompt("Enter how many grid you want ! ", 16)) ** 2;  
+    grid = Number(prompt("Enter how many grid you want ! ", 16));
+    if (grid > 100) {
+      makeGrid(100);
+      return;
+    }
+    makeGrid(grid);
   });
 });
 
-for (let i = 0; i < 256; i++) {
-  const squareGrid = document.createElement("div");
-  squareGrid.classList.add("grid");
-  squareGrid.setAttribute("id", i + 1);
-  squareContainer.appendChild(squareGrid);
-  squareGrid.addEventListener("mouseenter", () => {
-    squareGrid.style.backgroundColor = "black";
-  });
-}
+makeGrid(16);
